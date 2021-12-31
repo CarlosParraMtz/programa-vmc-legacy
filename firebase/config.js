cf3e6f9aa73dbe48c7fd74d502165ffd8b253540
ChayOne <chay.exe@outlook.com>
2021-12-31 12:03:05 -0600

    sec

---
+++ firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDkR0VszBS9CQl21qji3GRgIVhnWVM6VGc",
+  authDomain: "programa-vmc.firebaseapp.com",
+  projectId: "programa-vmc",
+  storageBucket: "programa-vmc.appspot.com",
+  messagingSenderId: "156679315806",
+  appId: "1:156679315806:web:6f0872f2a80f78df30afcf",
+  measurementId: "G-B5S2QDLFP2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
+export const analytics = getAnalytics(app);
