a6673ba2e7f5de5b1c548e329d17d43413d3e22c
Carlos Parra <86031209+ChayOne@users.noreply.github.com>
2022-06-21 19:09:01 -0500

    Update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_APIKEY,
-  authDomain: "programa-vmc.firebaseapp.com",
-  projectId: "programa-vmc",
-  storageBucket: "programa-vmc.appspot.com",
-  messagingSenderId: "156679315806",
-  appId: "1:156679315806:web:6f0872f2a80f78df30afcf",
-  measurementId: "G-B5S2QDLFP2"
+  authDomain: "programavmc.firebaseapp.com",
+  projectId: "programavmc",
+  storageBucket: "programavmc.appspot.com",
+  messagingSenderId: process.env.NEXT_PUBLIC_MSID,
+  appId: process.env.NEXT_PUBLIC_APPID,
+  measurementId: process.env.NEXT_PUBLIC_MEASID
 };
 
 // Initialize Firebase
