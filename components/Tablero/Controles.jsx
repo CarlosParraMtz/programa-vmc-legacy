e339437d5b59ec42dfa7a51d4b7460e2264b0fc1
Carlos Parra <86031209+ChayOne@users.noreply.github.com>
2022-07-05 16:42:22 -0500

    Interactividad pt. 1

--- components/Tablero/Controles.jsx
+++ components/Tablero/Controles.jsx
@@ -11,8 +11,12 @@ import AddIcon from '@mui/icons-material/Add';
 import NoteIcon from '@mui/icons-material/Note';
 import CloseIcon from '@mui/icons-material/Close'
 import EditIcon from '@mui/icons-material/Edit'
+import SaveIcon from '@mui/icons-material/Save'
+import CancelIcon from '@mui/icons-material/Cancel'
 
-export default function Controles() {
+export default function Controles({ useEditando }) {
+
+    const [editando, setEditando] = useEditando
 
     const btnStyle = { borderRadius: '0px' }
     const estiloBtn = { background: "#5b3c88", "&:hover": { background: "#6b4c88" } }
@@ -20,45 +24,79 @@ export default function Controles() {
 
     return (
         <Box sx={{ background: '#ddd', display: 'flex', mb: 1 }} >
-            <Tooltip title='Mes anterior' >
-                <span>
-                    <IconButton sx={btnStyle} size='large' >
-                        <ArrowBackIcon />
-                    </IconButton>
-                </span>
-            </Tooltip>
+            {
+                !editando
+                    ? (
+                        <>
+                            <Tooltip title='Mes anterior' >
+                                <span>
+                                    <IconButton sx={btnStyle} size='large' >
+                                        <ArrowBackIcon />
+                                    </IconButton>
+                                </span>
+                            </Tooltip>
 
-            <Tooltip title='Mes siguiente' >
-                <span>
-                    <IconButton sx={btnStyle} size='large' >
-                        <ArrowForwardIcon />
-                    </IconButton>
-                </span>
-            </Tooltip>
+                            <Tooltip title='Mes siguiente' >
+                                <span>
+                                    <IconButton sx={btnStyle} size='large' >
+                                        <ArrowForwardIcon />
+                                    </IconButton>
+                                </span>
+                            </Tooltip>
 
-            <Tooltip title='Agregar mes' >
-                <span>
-                    <IconButton sx={btnStyle} size='large'>
-                        <AddIcon />
-                    </IconButton>
-                </span>
-            </Tooltip>
+                            <Tooltip title='Agregar mes' >
+                                <span>
+                                    <IconButton sx={btnStyle} size='large'>
+                                        <AddIcon />
+                                    </IconButton>
+                                </span>
+                            </Tooltip>
 
-            <Tooltip title='Borrar mes' >
-                <span>
-                    <IconButton sx={btnStyle} size='large' >
-                        <DeleteIcon />
-                    </IconButton>
-                </span>
-            </Tooltip>
+                            <Tooltip title='Editar datos del mes' >
+                                <span>
+                                    <IconButton sx={btnStyle} size='large' onClick={() => setEditando(true)} >
+                                        <EditIcon />
+                                    </IconButton>
+                                </span>
+                            </Tooltip>
 
-            <Tooltip title='Generar asignaciones' >
-                <span>
-                    <IconButton sx={{...estiloBtn, position:'relative', top:10,ml:1, color:'white'}}>
-                        <NoteIcon/>
-                    </IconButton>
-                </span>
-            </Tooltip>
+                            <Tooltip title='Borrar mes' >
+                                <span>
+                                    <IconButton sx={btnStyle} size='large' >
+                                        <DeleteIcon />
+                                    </IconButton>
+                                </span>
+                            </Tooltip>
+
+                            <Tooltip title='Generar asignaciones' >
+                                <span>
+                                    <IconButton sx={{ ...estiloBtn, position: 'relative', top: 10, ml: 1, color: 'white' }}>
+                                        <NoteIcon />
+                                    </IconButton>
+                                </span>
+                            </Tooltip>
+                        </>
+                    ) : (
+                        <>
+                            <Tooltip title='Guardar' >
+                                <span>
+                                    <IconButton sx={btnStyle} size='large' onClick={() => setEditando(false)} >
+                                        <SaveIcon />
+                                    </IconButton>
+                                </span>
+                            </Tooltip>
+                            <Tooltip title='Cancelar' >
+                                <span>
+                                    <IconButton sx={btnStyle} size='large' onClick={() => setEditando(false)} >
+                                        <CancelIcon />
+                                    </IconButton>
+                                </span>
+                            </Tooltip>
+                        </>
+                    )
+            }
+
+
 
 
         </Box>
--- components/Tablero/Tablero.jsx
+++ components/Tablero/Tablero.jsx
@@ -1,73 +1,59 @@
 import { useState } from 'react'
-import {
-	Box,
-	Button,
-	Chip,
-	Divider,
-	Grid,
-	IconButton,
-	Stack,
-	Tooltip,
-	Typography,
-} from '@mui/material'
+import { Box, Button, IconButton, Input, Stack, Typography } from '@mui/material'
 import AddIcon from '@mui/icons-material/Add';
-
-import { useTheme } from '@mui/material/styles';
-import useMediaQuery from '@mui/material/useMediaQuery';
+import EditIcon from '@mui/icons-material/Edit';
 
 import Controles from './Controles';
 import FechaDeAsignaciones from './FechaDeAsignaciones';
+import { useEffect } from 'react';
 
 
 export default function Tablero() {
 
-	const theme = useTheme();
-	const esLG = useMediaQuery(theme.breakpoints.up('lg'));
-
-	const tableSM = 12
-	const tableXS = 12
-	const tableLG = 3
-
-	const [data, setData] = useState({
-		periodo: 'Julio 2022',
-		fechas: []
-	})
+	const [data, setData] = useState({ periodo: 'Julio 2022', fechas: [] })
+	const [editando, setEditando] = useState(false)
+	const [periodo, setPeriodo] = useState('')
 
 
 	const agregarFecha = () => {
 		let _data = { ...data }
-		_data.fechas.push({
-			fecha: '07-07-22',
-			asignaciones: []
-		})
+		_data.fechas.push({ fecha: '07-07-22', asignaciones: [] })
 		setData(_data)
 	}
+
+	const cambiarPeriodo = () => {
+		let _data = { ...data, periodo }
+		setData(_data)
+	}
+
+	useEffect(() => {
+		setPeriodo(data.periodo)
+	}, [data])
 
 
 	return (
 		<>
-			<Controles />
+			<Controles
+				useEditando={[editando, setEditando]}
+			/>
 
 
 			<Box sx={{ width: '100%', p: 2 }} id="tablero" >
 				<Stack spacing={2} >
-
-
-					<Typography variant='h4' >
-						<strong>
-							{data.periodo}
-						</strong>
-					</Typography>
-
-
 					{
-						data.fechas.map((fecha, indexFechas) => <FechaDeAsignaciones
-							key={indexFechas}
-							useData={[data, setData]}
-							indexFechas={indexFechas}
-						/>)
+						editando
+							? <Input type='text' sx={{ maxWidth: '400px' }} value={periodo} onChange={e => setPeriodo(e.target.value)} onBlur={cambiarPeriodo} />
+							: <Box sx={{display:'flex'}}>
+							<Typography variant='h4' ><strong>{data.periodo}</strong></Typography>
+							<IconButton onClick={()=>setEditando(true)} >
+								<EditIcon />
+							</IconButton>
+							</Box>
 					}
 
+
+
+					{data.fechas.map((fecha, indexFechas) => <FechaDeAsignaciones key={indexFechas} useData={[data, setData]} indexFechas={indexFechas} />)}
 
 					<Box sx={{ width: '100%' }} >
 						<Button
